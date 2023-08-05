94887bf86a978cb190cdb3a1011d27ed674f3e47
FaberJoo <sjdk0805@gmail.com>
2023-08-05 15:22:28 +0900

    chore: winstonLooger 적용 #9

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setSwagger } from './global/config/swagger';
+import { winstonLogger } from './global/config/winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+  });
   // swagger 적용
   setSwagger(app);
 
