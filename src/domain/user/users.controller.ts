ff75ea49b947d2bae14425b6060560586261fb17
FaberJoo <sjdk0805@gmail.com>
2023-08-05 15:22:48 +0900

    chore: logger test #9

--- src/domain/user/users.controller.ts
+++ src/domain/user/users.controller.ts
@@ -1,11 +1,15 @@
 import { TypedBody, TypedRoute } from '@nestia/core';
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { TestDto } from './dto/create-user.dto';
 
 @Controller('/users')
 export class UsersController {
   @TypedRoute.Post()
   createUser(@TypedBody() body: TestDto) {
+    Logger.error('hello', 'world');
+    Logger.warn('hello', 'world');
+    Logger.log('hello', 'world');
+    Logger.debug('hello', 'world');
     return `Hello World! ${body.age}`;
   }
 }
