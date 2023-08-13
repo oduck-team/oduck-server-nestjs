b806cc0c2112bd4cbb8cd90fd3215adb9f2d5913
FaberJoo <sjdk0805@gmail.com>
2023-08-13 11:01:38 +0900

    feat: redis session store 적용 #11

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,22 @@ import { setSwagger } from './global/config/swagger';
 import { winstonLogger } from './global/config/winston';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import RedisStore from 'connect-redis';
+import { redisClient } from './global/libs/redis';
+import { Logger } from '@nestjs/common';
+
+const TTL = 60 * 60 * 24 * 14; // 14일
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: winstonLogger,
   });
+
+  const redisStore = new RedisStore({
+    client: redisClient,
+    ttl: process.env.SESSION_TTL ? parseInt(process.env.SESSION_TTL) : TTL,
+  });
+
   // swagger 적용
   setSwagger(app);
 
@@ -18,11 +29,14 @@ async function bootstrap() {
 
   app.use(
     session({
-      secret: process.env.SESSION_SECRET!,
+      store: redisStore,
+      secret: process.env.SESSION_SECRET || 'secret',
       saveUninitialized: false,
       resave: false,
       cookie: {
-        maxAge: 600000, // 임시 10분
+        maxAge: process.env.SESSION_TTL
+          ? parseInt(process.env.SESSION_TTL) * 1000
+          : TTL * 1000, // 임시 10분
       },
     }),
   );
@@ -31,7 +45,15 @@ async function bootstrap() {
   app.use(passport.session());
 
   // 포트 설정후 실행
-  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+  await app
+    .listen(process.env.PORT ? parseInt(process.env.PORT) : 3000)
+    .then(() => {
+      Logger.log(
+        `Server listening on... ${
+          process.env.PORT ? parseInt(process.env.PORT) : 3000
+        } port`,
+      );
+    });
 }
 
 bootstrap();
