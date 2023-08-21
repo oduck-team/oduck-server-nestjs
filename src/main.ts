8e43c6f813ce6573b39c8c903656ad625a4e7110
sunday00tdi <102128587+sunday00tdi@users.noreply.github.com>
2023-08-21 21:42:48 +0900

    :sparkles: enable version

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { setSwagger } from './global/config/swagger';
 import { winstonLogger } from './global/config/winston';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,10 @@ async function bootstrap() {
 
   // helmet 헤더 보안 적용
   app.use(helmet());
+
+  app
+    .setGlobalPrefix('api')
+    .enableVersioning({ type: VersioningType.URI, defaultVersion: '20230821' });
 
   // 포트 설정후 실행
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
