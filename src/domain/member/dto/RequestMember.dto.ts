49176e3bcdfd5a8c3a26917d6c8efc6113772448
FaberJoo <sjdk0805@gmail.com>
2023-08-28 17:05:16 +0900

    refactor: MemberDto 변경 #20
    - 이름 변경 Dto 정규표현식 추가
    - 프로필 변경 Dto 추가
    - Count 포함 프로필 Dto 추가

--- src/domain/member/dto/RequestMember.dto.ts
+++ src/domain/member/dto/RequestMember.dto.ts
@@ -4,6 +4,19 @@ export interface UpdateNameDto extends Pick<MemberProfile, 'name'> {
   /**
    * @minLength 2
    * @maxLength 10
+   * @pattern ^[가-힣A-Za-z0-9]{2,10}$
+   * @example '홍길동'
    */
   name: string;
+}
+
+export interface UpdateProfileDto extends UpdateNameDto {
+  /**
+   * @minLength 0
+   * @maxLength 100
+   * @nullable
+   * @default null
+   * @example '안녕하세요'
+   */
+  info: string | null;
 }
