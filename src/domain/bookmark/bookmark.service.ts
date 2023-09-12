336a6d57671b9c5961e0d9cf52be77b8226e5343
FaberJoo <sjdk0805@gmail.com>
2023-09-12 22:11:15 +0900

    refactor: bookmarkService findBookmark 분리 #32

--- src/domain/bookmark/bookmark.service.ts
+++ src/domain/bookmark/bookmark.service.ts
@@ -10,10 +10,7 @@ export class BookmarkService {
   constructor(private readonly bookmarkRepository: BookmarkRepository) {}
 
   async createBookmark(memberId: number, aniId: number) {
-    const bookmark = await this.bookmarkRepository.findBookmark(
-      memberId,
-      aniId,
-    );
+    const bookmark = await this.findBookmark(memberId, aniId);
 
     if (bookmark) {
       throw new ConflictException('Already bookmarked');
@@ -22,15 +19,16 @@ export class BookmarkService {
     await this.bookmarkRepository.createBookmark(memberId, aniId);
   }
 
+  async findBookmark(memberId: number, aniId: number) {
+    return await this.bookmarkRepository.findBookmark(memberId, aniId);
+  }
+
   async findBookmarks(memberId: number, query) {
     return await this.bookmarkRepository.findBookmarks(memberId, query);
   }
 
   async deleteBookmark(memberId: number, aniId: number) {
-    const bookmark = await this.bookmarkRepository.findBookmark(
-      memberId,
-      aniId,
-    );
+    const bookmark = await this.findBookmark(memberId, aniId);
 
     if (!bookmark) {
       throw new ForbiddenException('Not bookmarked');
