9e4030f315a196db2cff3bfb25020056543ffac7
sunday00tdi <102128587+sunday00tdi@users.noreply.github.com>
2023-09-14 02:00:49 +0900

    :white_check_mark: test: modify test mock for relation

--- tests/unit/animation/animation.service.spec.ts
+++ tests/unit/animation/animation.service.spec.ts
@@ -1,10 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnimationService } from '../../../src/domain/animation/animation.service';
 import { AnimationRepository } from '../../../src/domain/animation/animation.repository';
-import { PrismaService } from '../../../src/global/database/prisma/prisma.service';
-import { Animation, BroadcastType, Rating, Status } from '@prisma/client';
+import { BroadcastType, GenreType, Rating, Status } from '@prisma/client';
+import { Role } from '.prisma/client';
+import { PrismaModule } from '../../../src/global/database/prisma/prisma.module';
 
-const MOCK_RESULT: Animation = {
+const MOCK_RESULT = {
   id: 1,
   name: '바키',
   plot: '',
@@ -13,13 +14,71 @@ const MOCK_RESULT: Animation = {
   rating: Rating.ADULT,
   primaryKeyword: '',
   status: Status.FINISHED,
-  isReleased: false,
   viewCount: 0,
   reviewCount: 0,
+  isReleased: false,
   imageUrl: 'src/baki.jpg',
   createdAt: new Date(),
   updatedAt: new Date(),
-  deletedAt: null,
+  deletedAt: new Date(),
+  studios: [
+    {
+      studio: {
+        id: 1,
+        name: '지브리스튱디오',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    },
+  ],
+  seasons: [
+    {
+      id: 1,
+      createdAt: new Date(),
+      animationId: 1,
+      year: 2023,
+      quarter: 1,
+    },
+  ],
+  genres: [
+    {
+      genre: {
+        id: 1,
+        createdAt: new Date(),
+        type: GenreType.FANTASY,
+      },
+    },
+  ],
+  voiceActors: [
+    {
+      voiceActor: {
+        id: 1,
+        name: '누구누구',
+        createdAt: new Date(),
+      },
+    },
+  ],
+  originalWorkers: [
+    {
+      originalWorker: {
+        id: 1,
+        name: '데즈카 오사무',
+        createdAt: new Date(),
+      },
+    },
+  ],
+};
+
+const MockUser = {
+  id: 1,
+  memberId: 1,
+  name: 'asd',
+  info: null,
+  role: Role.ADMIN,
+  imageUrl: null,
+  point: 1,
+  createdAt: new Date(),
+  updatedAt: new Date(),
 };
 
 describe('AnimationService', () => {
@@ -28,7 +87,8 @@ describe('AnimationService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService, AnimationService, AnimationRepository],
+      imports: [PrismaModule],
+      providers: [AnimationService, AnimationRepository],
     }).compile();
 
     repository = module.get<AnimationRepository>(AnimationRepository);
@@ -43,7 +103,7 @@ describe('AnimationService', () => {
     it('should return all animations', async () => {
       jest.spyOn(repository, 'getAnimations').mockResolvedValue([MOCK_RESULT]);
 
-      const result = await (async () => await service.getList({}))();
+      const result = await (async () => await service.getList(MockUser, {}))();
 
       expect(result[0].name).toContain('바키');
     });
@@ -53,7 +113,8 @@ describe('AnimationService', () => {
     it('should return an animation', async () => {
       jest.spyOn(repository, 'getAnimationById').mockResolvedValue(MOCK_RESULT);
 
-      const result = await (async () => await service.getOneById(1))();
+      const result = await (async () =>
+        await service.getOneById(MockUser, 1))();
 
       expect(result.name).toContain('바키');
     });
